a047670aaec1294b5a9595ad95bbc0dc65fa52a5
clrpauline <108258780+clrpauline@users.noreply.github.com>
2022-06-30 13:42:08 +0200

    Add files via upload

---
+++ Earth_temperature.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+import seaborn as sns
+sns.set_theme() 
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly
+import streamlit as st
+
+st.title ('Analyse et projections du dérèglement climatique global')
+
