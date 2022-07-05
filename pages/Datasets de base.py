775d3a7a525e6bb3f07306fd689754408fd65541
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 14:29:59 +0200

    Update Datasets de base.py

--- pages/Datasets de base.py
+++ pages/Datasets de base.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+sns.set_theme() 
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly
+import streamlit as st
+
 st.title("Datasets de base")
