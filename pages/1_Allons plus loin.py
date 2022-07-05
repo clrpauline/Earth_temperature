d7096436cbe650aba67eaa60c0d68fb40e87ea61
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 15:39:28 +0200

    Update 1_Allons plus loin.py

--- pages/1_Allons plus loin.py
+++ pages/1_Allons plus loin.py
@@ -18,12 +18,12 @@ df_zone = pd.read_csv("ressources/df_zone_kaggle.csv")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("DataFrame par Pays") 
+    st.write("DataFrame par Pays") 
     st.dataframe(df_country ,1000, 500)
             
 
 with col2:
-    st.header("DataFrame par Zone")    
+    st.write("DataFrame par Zone")    
     st.dataframe(df_zone, 1000, 500)
 
 
