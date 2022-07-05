1d4d32f252b5164c4446d41d3c7f363d8c1b9a94
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 17:33:21 +0200

    Update 3_Projections.py

--- pages/3_Projections.py
+++ pages/3_Projections.py
@@ -7,6 +7,11 @@ import plotly.graph_objects as go
 import plotly
 import streamlit as st
 
+st.set_page_config(
+    page_title="Projections",
+    page_icon="🌍",
+)
+
 st.title("Projections pour la fin du siècle")
 st.write("Cartes de projections des anomalies")
 st.write("Cartes de projections des pays invivables")
