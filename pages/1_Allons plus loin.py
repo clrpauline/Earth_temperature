2f13780835d8da900e380e3dfb39ea2489897440
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 16:08:56 +0200

    Update 1_Allons plus loin.py

--- pages/1_Allons plus loin.py
+++ pages/1_Allons plus loin.py
@@ -20,12 +20,14 @@ df_zone.rename(columns={'Country Code': 'Code'}, inplace=True)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("DataFrame par Pays") 
+    st.subheader("DataFrame par Pays")
+    st.write("Le DataFrame des anomalies dans tous les pays du monde de 1961 à 2015")
     st.dataframe(df_country ,1000, 500)
             
 
 with col2:
-    st.subheader("DataFrame par Zone")    
+    st.subheader("DataFrame par Zone")
+    st.write("Le DataFrame des anomalies classées par zones de 1961 à 2015")
     st.dataframe(df_zone, 1000, 500)
 
 
