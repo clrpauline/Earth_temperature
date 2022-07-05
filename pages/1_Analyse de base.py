6db6d09134b380a66b2e9b582efbea82c40c10de
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 17:50:01 +0200

    Update 1_Analyse de base.py

--- pages/1_Analyse de base.py
+++ pages/1_Analyse de base.py
@@ -15,11 +15,7 @@ st.set_page_config(
 
 st.write("# Analyse Macro du dérèglement climatique 🌍")
 
-dfg = pd.read_csv("ressources/ZonAnn.Ts+dSST.csv", header=1, index_col=(0))
-dfg = dfg.replace(to_replace='***', value=np.nan)
-dfg = dfg.drop(["J-D","D-N", "DJF", "MAM", "JJA", "SON"], axis=1)
-
-st.write(dfg)
+st.write(df)
 
 st.markdown(
     """
