d3b8593c7f214ee28dfc278efd606b2a2bcd6a73
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 15:24:53 +0200

    Update 1_Allons plus loin.py

--- pages/1_Allons plus loin.py
+++ pages/1_Allons plus loin.py
@@ -12,7 +12,7 @@ st.write("Introduction du dataframe Kaggle")
 st.write("Graph additionnels kaggle: Boxplot, Relplot, polarplot...")
 
 #Chargement des dataframes Kaggle:
-df_country = pd.read_csv("ressources/df_country_kaggle.csv")
+df_country = pd.read_csv("ressources/df_country_kaggle.csv", width=700, height=768)
 df_zone = pd.read_csv("ressources/df_zone_kaggle.csv")
 
 st.dataframe(df_country)
