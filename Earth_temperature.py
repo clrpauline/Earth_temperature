8cf9d21a35cb558ddcf72a2de7ea507450408306
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-05 18:05:14 +0200

    Update Earth_temperature.py

--- Earth_temperature.py
+++ Earth_temperature.py
@@ -14,3 +14,4 @@ st.set_page_config(
     page_icon="🌍",
 )
 st.title("Etude du réchauffement climatique de 1880 à 2100")
+st.write("Dans cette étude vous sera présenté une vision objective et factuelle du réchauffement climatique, du début de la révolution industrielle, jusqu'à la fin du XXIe siècle. Les données interactives et visuelles proviennent de mesures de la NASA, plus particulièrement du GISS (Goddard Institute for Space Studies).")
