da7ee1ebb3b26a870ff5baef2d43d0650f4262bf
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-06 13:56:37 +0200

    Update 3_Projections.py

--- pages/3_Projections.py
+++ pages/3_Projections.py
@@ -56,10 +56,13 @@ if results == "RCP2,6: Scénario optimiste":
     st.plotly_chart(fig1)
 elif results == "RCP4,5: Scénario moyen bas":
     st.subheader("Projection de l'invivabilité des pays d'ici 2100")
+    st.write("Avec le scénario RCP4,5")
     st.plotly_chart(fig2)
 elif results == "RCP6: Scénario moyen haut":
     st.subheader("Projection de l'invivabilité des pays d'ici 2100")
+    st.write("Avec le scénario RCP6")
     st.plotly_chart(fig3)
 elif results == "RCP8,5: Scénario pessimiste":
     st.subheader("Projection de l'invivabilité des pays d'ici 2100")
+    st.write("Avec le scénario RCP8,5")
     st.plotly_chart(fig4)
