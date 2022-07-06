764aa71bd33bb85bb09bb44f9cdf44283080c7b5
Chounyx <108731529+Chounyx@users.noreply.github.com>
2022-07-06 10:14:45 +0200

    Update 3_Projections.py

--- pages/3_Projections.py
+++ pages/3_Projections.py
@@ -21,12 +21,29 @@ st.write("Cartes de projections des pays invivables")
 st.subheader("Projection de l'invivabilité des pays d'ici 2100")
 
 df26=pd.read_csv("ressources/Dataframe_HI_2_6.csv")
+df45=pd.read_csv("ressources/Dataframe_HI_4_5.csv")
+df6=pd.read_csv("ressources/Dataframe_HI_6.csv")
+df85=pd.read_csv("ressources/Dataframe_HI_8_5.csv")
 
 C1, C2 = st. columns(2)
 
 
-fig = px.choropleth(df26, locations="Country", color="Seuil",range_color=[0,200],
+fig1 = px.choropleth(df26, locations="Country", color="Seuil",range_color=[0,200],
                   color_discrete_sequence=["green","orange","red"],
-                    title="Invivabilité des pays avec le scénario RCP 2.6",
-                    locationmode='country names',color_continuous_midpoint=0)
-st.plotly_chart(fig)
+                   locationmode='country names',color_continuous_midpoint=0)
+st.plotly_chart(fig1)
+
+fig2 = px.choropleth(df45, locations="Country", color="Seuil",range_color=[0,200],
+                  color_discrete_sequence=["green","orange","red"],
+                   locationmode='country names',color_continuous_midpoint=0)
+st.plotly_chart(fig2)
+
+fig3 = px.choropleth(df6, locations="Country", color="Seuil",range_color=[0,200],
+                  color_discrete_sequence=["green","orange","red"],
+                   locationmode='country names',color_continuous_midpoint=0)
+st.plotly_chart(fig3)
+
+fig4 = px.choropleth(df85, locations="Country", color="Seuil",range_color=[0,200],
+                  color_discrete_sequence=["green","orange","red"],
+                   locationmode='country names',color_continuous_midpoint=0)
+st.plotly_chart(fig4)
