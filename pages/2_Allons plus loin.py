d890c4ece3c13d6bf18558b2160770d61f7d26e5
clrpauline <108258780+clrpauline@users.noreply.github.com>
2022-07-07 20:00:04 +0200

    Update 2_Allons plus loin.py

--- pages/2_Allons plus loin.py
+++ pages/2_Allons plus loin.py
@@ -67,11 +67,11 @@ else :
      f=0
      j=0
      for i in uniques:
-       if f<= 16:
+       if f< 17:
             df_country.loc[df_country['Country Name']==i, 'x']=x[j]
             df_country.loc[df_country['Country Name']==i, 'y']=y[f]
             f+=1
-       if f==17:
+       else :
             df_country.loc[df_country['Country Name']==i, 'x']=x[j]
             df_country.loc[df_country['Country Name']==i, 'y']=y[f]
             f=0
