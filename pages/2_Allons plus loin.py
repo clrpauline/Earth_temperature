d6ca6a6be18c7a36ca0c45e767758b643545edde
clrpauline <108258780+clrpauline@users.noreply.github.com>
2022-07-07 15:00:27 +0200

    Update 2_Allons plus loin.py

--- pages/2_Allons plus loin.py
+++ pages/2_Allons plus loin.py
@@ -7,57 +7,45 @@ import plotly.graph_objects as go
 import plotly
 import streamlit as st
 
-st.set_page_config(
-    page_title="Allons plus loin",
-    page_icon="🌍",
-)
-st.title("Comparaison avec d'autres données")
-
-st.write("Graph additionnels kaggle: Boxplot, Relplot, polarplot...")
-st.write("Pour aller plus loin, il nous paraît important d'aller étudier les anomalies sur une maille plus fine. Nous avons donc trouvé des données qui proviennent de la World Bank et qui nous permettent d'affiné notre étude:")
-#Chargement des dataframes Kaggle:
-df_country = pd.read_csv("ressources/df_country_kaggle.csv")
-df_zone = pd.read_csv("ressources/df_zone_kaggle.csv")
-df_country.rename(columns={'Country Code': 'Code'}, inplace=True)
-df_zone.rename(columns={'Country Code': 'Code'}, inplace=True) 
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.subheader("Données par pays")
-    st.write("Un dataframe répertoriant l'ensemble des anomalies, dans tous les pays, de 1961 à 2015")
-    st.dataframe(df_country ,1000, 250)
-            
-
-with col2:
-    st.subheader("Données par zones")
-    st.write("Un DataFrame répertoriant les anomalies, classées par zones, de 1961 à 2015")
-    st.dataframe(df_zone, 1000, 250)
+st.markdown("<h1 style='text-align: center; '>Analyse des anomalies de température par zones et pays 🌍</h1>", unsafe_allow_html=True)
 
 
-st.subheader("Visualisation des données")    
-df_zone=pd.read_csv('ressources/df_zone.csv')
-fig = px.bar_polar(df_zone, r="Anomalies", theta="Country Name", color="Anomalies", animation_frame="year",
-                   animation_group="Country Name" , color_continuous_scale= 'RdYlBu_r', color_continuous_midpoint=0, 
-                   range_r=[df_zone["Anomalies"].min(), df_zone["Anomalies"].max()],
-                    range_color=[df_zone["Anomalies"].min(), df_zone["Anomalies"].max()],labels={"Anomalies": "Anomaly<br>in °C"}
-                   )
-fig.update_layout(title_text="Anomaly per zone from 1961 to 2019 <br><sup> Period Reference : mean 1951-1980")
+st.write("Pour aller plus loin, nous avons ainsi récupéré un jeu de données rescensant les anomalies de température par continents et par pays de 1961 à 2019. Ces anomalies sont comparées à la moyenne de température entre 1951 et 1980.")
 
+genre = st.radio('',
+     ('Par Continents ', 'Par Pays'))
 
-fig.update_polars(angularaxis_dtick=1, 
-                  angularaxis_exponentformat="power")
-
-st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
-
-fig = go.Figure()
-fig = px.bar(df_zone, x="Country Name", y='Anomalies', animation_frame="year",animation_group="Country Name" ,range_y=[-2,3], 
+if genre == 'Par Continents ':
+     st.markdown("<h3 style='text-align: center; '>Analyse des anomalies de température par zones de 1961 à 2019 </h3>", unsafe_allow_html=True)   
+     df_zone=pd.read_csv('ressources/df_zone.csv')
+     fig = go.Figure()
+     fig = px.bar(df_zone, x="Country Name", y='Anomalies', animation_frame="year",animation_group="Country Name" ,range_y=[-2,3], 
              color='Anomalies',color_continuous_scale='RdYlBu_r' , orientation="v",color_continuous_midpoint=0, 
              range_color=[df_zone["Anomalies"].min(), df_zone["Anomalies"].max()],labels={"Anomalies": "Anomaly<br>in °C"}
              )
-fig.update_xaxes(
+     fig.update_xaxes(
         tickangle = 45,
         title_text = None)
 
-fig.update_layout(title_text="Anomaly per zone from 1961 to 2019 <br><sup> Period Reference : mean 1951-1980")
-st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
+     fig.update_layout(title_text="Anomaly per zone from 1961 to 2019 <br><sup> Period Reference : mean 1951-1980")
+     st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
+     st.markdown("""
+     Une fois de plus la tendance est claire, plus nous avançons dans le temps, plus les anomalies augmentent en températures et ce dans l’ensemble des zones géographiques. 
+     Ce graphique met aussi en avant les zones où les anomalies semblent progresser plus rapidement que la moyenne mondiale (Europe et Afrique).
+     """)
+else :
+     st.markdown("<h3 style='text-align: center; '>Analyse des anomalies de température par pays de 1961 à 2019 </h3>", unsafe_allow_html=True)   
+
+     df=pd.read_csv('ressources/Temperature_change_Data.csv')
+     df=df.replace({"URSS": "Russia",})
+     df=df.rename({'tem_change':'Anomalies'}, axis=1)
+     fig = px.choropleth(df, locations="Country Name", color="Anomalies",
+                    color_continuous_scale='turbo', animation_group="Country Name",
+                    locationmode='country names', animation_frame="year", color_continuous_midpoint=0, 
+                   range_color=[-2, 3]
+             )
+     fig.update_layout(title_text="Anomaly from 1961 to 2019 <br><sup> Period Reference : mean 1951-1980")
+     st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
+     st.markdown("""Il est ainsi évident qu’en 50 ans, nous avons des anomalies de températures qui ont radicalement évoluées avec en 1969 des anomalies autour de 0°C tandis qu’en 2019, ces dernières se situent autour des 1°C à 2°C selon les pays, suivant ainsi la courbe globale de l’évolution des anomalies de température. On peut ainsi également observer que les pays de l’hémisphère Nord ont plus de variation de température que les pays de l’hémisphère Sud.""")
+
+st.markdown('Etant donné la croissance de ces anomalies, engendrant des températures extrêmes, une question importante pour les prochaines années se pose : il y aura-t-il des pays définis comme invivables d’ici quelques années ?')
